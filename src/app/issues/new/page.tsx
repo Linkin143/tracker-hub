027ec89c801434dd02461db1c5cdf928db0116ae
Linkin143 <kumarprince143p143@gmail.com>
2025-07-24 20:06:11 +0530

    New issue page created

---
+++ src/app/issues/new/page.tsx
@@ -0,0 +1,20 @@
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+
+const NewIssuePage = () => {
+    return (
+        <div className="max-w-xl space-y-3">
+            <TextField.Root placeholder="Title">
+                <TextField.Slot>
+                    <MagnifyingGlassIcon height="16" width="16" />
+                </TextField.Slot>
+            </TextField.Root>
+
+            <TextArea placeholder="Description" />
+            <Button>Submit New Issue</Button>
+
+        </div>
+    )
+}
+
+export default NewIssuePage;
--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuesPage = () => {
   return (
-    <div><Button>Issues Button</Button></div>
+    <div>
+      <Button>
+    <Link href="/issues/new">
+      New Issue
+    </Link>
+    </Button>
+    </div>
   )
 }
 
