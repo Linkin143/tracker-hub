6e06636aa2dda1f13c95fa0898a73f3a3e7fd238
Linkin143 <kumarprince143p143@gmail.com>
2025-07-26 20:58:01 +0530

    styling issue details

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { PrismaClient } from "@/generated/prisma";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 const prisma = new PrismaClient();
 
@@ -25,10 +27,14 @@ const IssueDetailPage = async ({ params }: Props) => {
 
     return (
         <div>
-            <p>{IssueDetail.title}</p>
-            <p>{IssueDetail.description}</p>
-            <p>{IssueDetail.status}</p>
-            <p>{IssueDetail.createdAt.toDateString()}</p>
+            <Heading>{IssueDetail.title}</Heading>
+            <Flex className="space-x-3 " my="2">
+                <IssueStatusBadge status={IssueDetail.status}></IssueStatusBadge>
+                <Text>{IssueDetail.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card>
+                <p>{IssueDetail.description}</p>
+            </Card>
 
         </div>
     )
