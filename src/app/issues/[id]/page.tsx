1c9db52d3ec720736459fabc995e068e3941d269
Linkin143 <kumarprince143p143@gmail.com>
2025-07-31 01:49:56 +0530

    Edit Btn Added

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,6 +1,8 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { PrismaClient } from "@/generated/prisma";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
 
@@ -28,8 +30,9 @@ const IssueDetailPage = async ({ params }: Props) => {
 
 
     return (
-        <div>
-            <Heading>{IssueDetail.title}</Heading>
+        <Grid columns={{initial:"1", md:"2"}} gap="5">
+            <Box>
+                <Heading>{IssueDetail.title}</Heading>
             <Flex className="space-x-3 " my="2">
                 <IssueStatusBadge status={IssueDetail.status}></IssueStatusBadge>
                 <Text>{IssueDetail.createdAt.toDateString()}</Text>
@@ -37,8 +40,15 @@ const IssueDetailPage = async ({ params }: Props) => {
             <Card className="prose" mt="4">
                 <ReactMarkdown>{IssueDetail.description}</ReactMarkdown>
             </Card>
+            </Box>
+            <Box>
+                <Button className="Hover:cursor-pointer">
+                    <Pencil2Icon />
+                <Link href={`/issues/${IssueDetail.id}/edit`}> Edit Issue</Link>
+                </Button>
+            </Box>
 
-        </div>
+        </Grid>
     )
 }
 
