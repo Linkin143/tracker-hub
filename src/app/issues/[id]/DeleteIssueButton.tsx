f863749a8306634e10cb35fd2e35a67b03fed642
Linkin143 <kumarprince143p143@gmail.com>
2025-09-28 09:07:26 +0530

    handle errors

--- src/app/issues/[id]/DeleteIssueButton.tsx
+++ src/app/issues/[id]/DeleteIssueButton.tsx
@@ -4,19 +4,44 @@ import { TrashIcon } from "@radix-ui/react-icons";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
 
     const router = useRouter()
+    const [error, setError] = useState(false);
 
     const handleDelete = async () => {
-        await axios.delete(`/api/issues/${issueId}`);
-        router.push("/issues");
-        router.refresh();
+        try {
+            
+            await axios.delete(`/api/issues/${issueId}`);
+            router.push("/issues");
+            router.refresh();
+        } catch (error) {
+            setError(true)
+            console.error(error);
+        }
     }
 
     return (
         <>
+
+            <AlertDialog.Root open={error}>
+                <AlertDialog.Content maxWidth="450px">
+                    <AlertDialog.Title>error Occured</AlertDialog.Title>
+                    <AlertDialog.Description size="2">
+                        Ooo! Unable to delete the issue. Please try again.
+                    </AlertDialog.Description>
+                    <AlertDialog.Cancel>
+                        <Button mt="4" variant="soft" color="gray" className="hover:cursor-pointer" onClick={() => setError(false)}>
+                            ok
+                        </Button>
+                    </AlertDialog.Cancel>
+
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+
+
             <AlertDialog.Root>
                 <AlertDialog.Trigger>
                     <Button className="hover:cursor-pointer" color="red">
@@ -29,6 +54,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
                     <AlertDialog.Description size="2">
                         Are you sure you want to delete this Issue? This action is permanent and cannot be undone.
                     </AlertDialog.Description>
+
 
                     <Flex gap="3" mt="4" justify="end">
                         <AlertDialog.Cancel>
